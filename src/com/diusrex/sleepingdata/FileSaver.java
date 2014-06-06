af565ecbb44524e3e82c803f02afd8e04e607ef1
Diusrex <killakan002@hotmail.com>
2014-06-05 21:49:30 -0600

    Improved FileSaver to save prompts.

--- src/com/diusrex/sleepingdata/FileSaver.java
+++ src/com/diusrex/sleepingdata/FileSaver.java
@@ -3,6 +3,7 @@ package com.diusrex.sleepingdata;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.List;
 
 import com.diusrex.sleepingdata.FileAccessor.NoAccessException;
 
@@ -12,20 +13,50 @@ public class FileSaver {
     static String DATA_FOLDER = "";
     static String PROMPTS_FOLDER = ".Prompts";
     
-    static public boolean WriteData(String dataFile, DataContainer data) throws IOException
+    static public boolean WriteData(String inputGroup, DataContainer data) throws IOException
     {
-        File loadFile = null;
+        File saveFile = null;
         
         try {
-            loadFile = FileAccessor.OpenFile(DATA_FOLDER, dataFile);
+            saveFile = FileAccessor.OpenFile(DATA_FOLDER, inputGroup);
         } catch(NoAccessException e) {
             return false;
         }
         
         try {
-            FileWriter writer = new FileWriter(loadFile, true);
+            FileWriter writer = new FileWriter(saveFile, true);
             
             for (String outputItem : data.itemsLoaded) {
+                writer.write(outputItem + ", ");
+            }
+            
+            writer.write("\n");
+            
+            
+            writer.close();
+            
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        }
+        
+        return true;
+    }
+    
+    static public boolean SavePrompts(String inputGroup, List<String> prompts)
+    {
+        File saveFile = null;
+        
+        try {
+            saveFile = FileAccessor.OpenFile(DATA_FOLDER, inputGroup);
+        } catch(Exception e) {
+            return false;
+        }
+        
+        try {
+            FileWriter writer = new FileWriter(saveFile, false);
+            
+            for (String outputItem : prompts) {
                 writer.write(outputItem + ", ");
             }
             
