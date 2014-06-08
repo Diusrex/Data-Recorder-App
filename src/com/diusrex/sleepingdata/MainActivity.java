66b98547a9ed5a3aad6dc8612491f9b527a59a3d
Diusrex <killakan002@hotmail.com>
2014-06-08 16:09:28 -0600

    MainActivity will now add the wanted InputGroups to availableInputGroupsPreference.

--- src/com/diusrex/sleepingdata/MainActivity.java
+++ src/com/diusrex/sleepingdata/MainActivity.java
@@ -1,19 +1,21 @@
 package com.diusrex.sleepingdata;
 
 import java.util.Arrays;
-import java.util.List;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 
 
 public class MainActivity extends ActionBarActivity {
-    static String LOG_TAG = "MainActivity";
+    static final String LOG_TAG = "MainActivity";
+    
     
     // Manages key valued pairs associated with stock symbols
       // Will be stored using name of save type for both value and key
@@ -29,6 +31,14 @@ public class MainActivity extends ActionBarActivity {
 		availableInputGroupsPreference = getSharedPreferences("availableInputGroups", MODE_PRIVATE);
 		
 		inputGroups = availableInputGroupsPreference.getAll().keySet().toArray(new String[0]);
+		
+		// Sort the stocks in alphabetical order
+        Arrays.sort(inputGroups, String.CASE_INSENSITIVE_ORDER);
+        
+        for (int i = 0; i < inputGroups.length; ++i) {
+            Log.w(LOG_TAG, "Hello" + i);
+            insertInputGroupInScrollView(inputGroups[i], i);
+        }
 	}
 	
 	
@@ -37,8 +47,32 @@ public class MainActivity extends ActionBarActivity {
 	    Intent intent = new Intent(this, InputNewGroupNameActivity.class);
 	    intent.putExtra(InputNewGroupNameActivity.PREVIOUS_INPUT_GROUPS, inputGroups);
 
-	    startActivity(intent);
+	    startActivityForResult(intent, InputNewGroupNameActivity.REQUEST_CODE);
 	}
+	
+	@Override
+	public void onActivityResult(int requestCode, int resultCode, Intent data) {
+	  super.onActivityResult(requestCode, resultCode, data);
+	  switch(requestCode) {
+	    case (InputNewGroupNameActivity.REQUEST_CODE) : {
+	      if (resultCode == Activity.RESULT_OK) {
+	        String newName = data.getStringExtra(InputNewGroupNameActivity.NAME_OF_INPUT_GROUP);
+	        saveNewInputGroup(newName);
+	        
+	      }
+	      
+	      break;
+	    } 
+	  }
+	}
+	
+    void saveNewInputGroup(String newInputGroup) {
+        SharedPreferences.Editor preferencesEditor = availableInputGroupsPreference.edit();
+        preferencesEditor.putString(newInputGroup, newInputGroup);
+        preferencesEditor.apply();
+        
+        updateInputGroupsList(newInputGroup);
+    }
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
