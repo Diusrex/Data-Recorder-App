413e153ecde31c07b95dc8d05ca425672517f3e8
Diusrex <killakan002@hotmail.com>
2014-06-11 21:19:14 -0600

    User can now choose the position they want to place new prompt in.

--- src/com/diusrex/sleepingdata/PromptSettingActivity.java
+++ src/com/diusrex/sleepingdata/PromptSettingActivity.java
@@ -5,15 +5,19 @@ import java.util.ArrayList;
 import java.util.List;
 
 import android.app.Activity;
+import android.app.ActionBar.LayoutParams;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.PopupWindow;
 import android.widget.TableLayout;
 import android.widget.TextView;
 
@@ -30,6 +34,9 @@ public class PromptSettingActivity extends Activity {
     List<EditText> inputs;
     
     String inputGroupName;
+    
+    PopupWindow positionPopup;
+    PopupWindow errorPopup;
     
     boolean isNew;
     
@@ -61,22 +68,52 @@ public class PromptSettingActivity extends Activity {
             isNew = false;
             
             for (String item : existingInputs) {
-                addPrompt(item);
+                addPromptToEnd(item);
             }
             
         } else {
             isNew = true;
             
-            addPrompt("");
+            addPromptToEnd("");
         }
     }
     
     public void addPrompt(View view)
     {
-        addPrompt("");
+        // Need to create a popup to ask about the position.
+        LayoutInflater layoutInflater = (LayoutInflater) getBaseContext().getSystemService(LAYOUT_INFLATER_SERVICE);
+        
+        View popupView = layoutInflater.inflate(R.layout.prompt_position_popup, null);
+        
+        positionPopup = new PopupWindow(popupView, LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
+        positionPopup.setFocusable(true);
+        
+        TextView warningText = (TextView) popupView.findViewById(R.id.infoText);
+        String output = getString(R.string.prompt_position_pt1);
+        
+        warningText.setText(String.format(output, inputs.size()));
+        
+        Button confirmButton = (Button) popupView.findViewById(R.id.confirmButton);
+        confirmButton.setOnClickListener(choosePositionListener);
+        
+        Button cancelButton = (Button) popupView.findViewById(R.id.cancelButton);
+        cancelButton.setOnClickListener(new View.OnClickListener() {
+            
+            @Override
+            public void onClick(View v) {
+                positionPopup.dismiss();
+            }
+        });
+        
+        positionPopup.showAtLocation(view, Gravity.CENTER, 0, 0);
     }
     
-    private void addPrompt(String enteredText)
+    private void addPromptToEnd(String enteredText)
+    {
+        addPromptToPosition(enteredText, inputs.size());
+    }
+    
+    private void addPromptToPosition(String enteredText, int position)
     {
         // Get the LayoutInflator service
         LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
@@ -85,21 +122,88 @@ public class PromptSettingActivity extends Activity {
         View newPromptRow = inflater.inflate(R.layout.prompt_enter_row, null);
         
         TextView number = (TextView) newPromptRow.findViewById(R.id.number);
-        number.setText("" + inputs.size() + ": ");
+        number.setText("" + (position + 1) + ": ");
         
         // Set up the EditText
         EditText newET = (EditText) newPromptRow.findViewById(R.id.input);
         newET.setText(enteredText);
         
-        // Add the newET to the inputs, and to the table
+        inputs.add(position, newET);
+        promptSettingTable.addView(newPromptRow, position);
         
-        // TODO: The position that the ET should be added to should be able to be changed. Also, will need to prompt to ask what output should be added.
-        inputs.add(newET);
-        promptSettingTable.addView(newPromptRow);
+        updateLaterPositionNumbers(position);
     }
+    
+    private void updateLaterPositionNumbers(int position)
+    {
+        for (int i = position + 1; i < inputs.size(); ++i)
+        {
+            View currentPromptRow = promptSettingTable.getChildAt(i);
+            TextView number = (TextView) currentPromptRow.findViewById(R.id.number);
+            
+            number.setText("" + (i + 1) + ": ");
+        }
+    }
+        
+    final View.OnClickListener choosePositionListener = new View.OnClickListener() {        
+        @Override
+        public void onClick(View v) {
+            View parentView = (View) v.getParent();
+            EditText numberInput = (EditText) parentView.findViewById(R.id.positionChosen);
+            
+            
+            String numberInputString = numberInput.getText().toString();
+            int wantedPosition = -1;
+            
+            try {
+                wantedPosition = Integer.parseInt(numberInputString);
+            } catch (NumberFormatException e) {
+                View buttonView = parentView.findViewById(R.id.confirmButton);
+                createPromptPositionErrorPopup(buttonView);
+            }
+            
+            if (wantedPosition >= 0 && wantedPosition <= inputs.size())
+            {
+                addPromptToPosition("", wantedPosition);
+                positionPopup.dismiss();
+            } else {
+                View buttonView = parentView.findViewById(R.id.confirmButton);
+                createPromptPositionErrorPopup(buttonView);
+            }
+        }
+        
+        private void createPromptPositionErrorPopup(View parentButton)
+        {
+            LayoutInflater layoutInflater = (LayoutInflater) getBaseContext().getSystemService(LAYOUT_INFLATER_SERVICE);
+            
+            View popupView = layoutInflater.inflate(R.layout.error_popup, null);
+            
+            errorPopup = new PopupWindow(popupView, LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
+            
+            TextView warningText = (TextView) popupView.findViewById(R.id.warningText);
+            warningText.setText(getString(R.string.prompt_position_invalid));
+            
+            Button dismissButton = (Button) popupView.findViewById(R.id.dismissButton);
+            dismissButton.setOnClickListener(new View.OnClickListener() {
+                
+                @Override
+                public void onClick(View v) {
+                    errorPopup.dismiss();
+                }
+            });
+            
+            errorPopup.showAtLocation(popupView, Gravity.CENTER, 0, 50);
+        }
+    };
+    
+    
     
     public void continueButtonClicked(View view)
     {
+        if (positionPopup != null) {
+            positionPopup.dismiss();
+        }
+        
         List<String> prompts = new ArrayList<String>();
         
         for (EditText text : inputs)
@@ -119,7 +223,16 @@ public class PromptSettingActivity extends Activity {
     
     public void cancelButtonClicked(View view)
     {
+        if (positionPopup != null) {
+            positionPopup.dismiss();
+        }
+        
+        if (errorPopup != null) {
+            errorPopup.dismiss();
+        }
+        
         setResult(RESULT_CANCELED, new Intent());
+        
         finish();
     }
     
