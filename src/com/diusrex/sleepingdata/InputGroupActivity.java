071344c22f58d2cf2084fb258462e9c5144c21cf
Diusrex <killakan002@hotmail.com>
2014-06-20 21:30:48 -0600

    Set up the information display for InputGroupActivity.

--- src/com/diusrex/sleepingdata/InputGroupActivity.java
+++ src/com/diusrex/sleepingdata/InputGroupActivity.java
@@ -3,7 +3,9 @@ package com.diusrex.sleepingdata;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
 
 public class InputGroupActivity extends Activity {
     static public String INPUT_GROUP_NAME = "InputGroupName";
@@ -26,8 +28,52 @@ public class InputGroupActivity extends Activity {
         if (enterPrompts) {
             runPromptSetting();
         }
+        
+        setUpInformation();
     }
     
+    @Override
+    public void onResume() {
+        super.onResume();
+        setUpInformation();
+    }
+    
+    void setUpInformation()
+    {
+        String groupNameInfo = getString(R.string.current_input_group);
+
+        TextView inputGroupNameTV = (TextView) findViewById(R.id.inputGroupName);
+        inputGroupNameTV.setText(String.format(groupNameInfo, inputGroupName));
+        
+        
+        int numberOfPrompts = FileLoader.numberOfPrompts(inputGroupName);
+        
+        String numberOfPromptsOutput;
+        
+        if (numberOfPrompts == 1) {
+            numberOfPromptsOutput = getString(R.string.input_group_num_prompts_single);
+        } else {
+            numberOfPromptsOutput = getString(R.string.input_group_num_prompts_multiple);
+            numberOfPromptsOutput = String.format(numberOfPromptsOutput, numberOfPrompts);
+        }
+        
+        TextView numberPromptsInfo = (TextView) findViewById(R.id.numberOfPrompts);
+        numberPromptsInfo.setText(numberOfPromptsOutput);
+        
+        
+        int numberOfDataRows = FileLoader.numberOfDataRows(inputGroupName);
+        String numberOfDataRowsOutput;
+        
+        if (numberOfDataRows == 1) {
+            numberOfDataRowsOutput = getString(R.string.input_group_num_data_rows_single);
+        } else {
+            numberOfDataRowsOutput = getString(R.string.input_group_num_data_rows_multiple);
+            numberOfDataRowsOutput = String.format(numberOfDataRowsOutput, numberOfDataRows);
+        }
+
+        TextView numberDataRowsInfo = (TextView) findViewById(R.id.numberOfDataRows);
+        numberDataRowsInfo.setText(numberOfDataRowsOutput);
+    }
     
     public void changeButtonClicked(View view) {
         runPromptSetting();
