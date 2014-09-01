7afe4e1b485a321580d3dfb637129f08ac16fa18
Diusrex <killakan002@hotmail.com>
2014-08-31 21:30:50 -0600

    Created the Help Activity.

---
+++ src/com/diusrex/sleepingdata/HelpActivity.java
@@ -0,0 +1,15 @@
+package com.diusrex.sleepingdata;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class HelpActivity extends Activity {
+    static final String LOG_TAG = "MainActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_help);
+    }
+}
